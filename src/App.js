7a9e4099e428e156302f6b7de582b2e9bceee12e
Sunny Gupta <sunny.gupta@globallogic.com>
2022-08-18 22:17:55 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 
 
-
 import React from 'react';
 import store from './store/index';
 import addArticle from './actions/index';
 import MainApp from './components/main';
 import { ADD_ARTICLE } from "./constants/action-types";
 
-
 function App() {
-
-
 
   store.subscribe(() => console.log('Look ma, Redux!!', store.getState()));
   // first way
   // store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1 }) );
 
-
   // second way
   const addItemToCart = () => {
     return {
-        type: ADD_ARTICLE,
-        payload: { title: 'React Redux Tutorial for Beginners', id: 1 }
-        }
+      type: ADD_ARTICLE,
+      payload: { title: 'React Redux Tutorial for Beginners', id: 1 }
     }
-
+  }
   return (
     <div className="App">
-
-
       <div>
- 
-
-      <MainApp/>
-       
-      <button onClick = {() => store.dispatch(addItemToCart())}>Add to cart</button>
-
-
-
+        <MainApp />
+        <button onClick={() => store.dispatch(addItemToCart())}>Add to cart</button>
       </div>
     </div>
   );
 
-
-
 }
-
 export default App;
-
-
-
-
-
-
-
-
-
-
